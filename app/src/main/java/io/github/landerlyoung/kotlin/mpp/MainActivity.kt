491290cd0dd145186d80db7018af9d615851f78f
landerlyoung <landerlyoung@gmail.com>
2018-11-08 17:06:39 +0800

    android module works fine

--- app/src/main/java/io/github/landerlyoung/kotlin/mpp/MainActivity.kt
+++ app/src/main/java/io/github/landerlyoung/kotlin/mpp/MainActivity.kt
@@ -2,11 +2,14 @@ package io.github.landerlyoung.kotlin.mpp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.text).text = createApplicationScreenMessage()
     }
 }
