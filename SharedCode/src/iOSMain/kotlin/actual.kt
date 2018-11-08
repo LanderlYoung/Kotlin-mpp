bfe261f261d2f98877e875a34aa9ca7e8331f7a5
landerlyoung <landerlyoung@gmail.com>
2018-11-08 17:26:27 +0800

    add iosFramework generate logic

--- SharedCode/src/iOSMain/kotlin/actual.kt
+++ SharedCode/src/iOSMain/kotlin/actual.kt
@@ -3,7 +3,7 @@ package io.github.landerlyoung.kotlin.mpp
 import platform.UIKit.UIDevice
 
 actual fun platformName(): String {
-    return UIDevice.currentDevices.let {
-        it.systenName() + " " + it.systemVersion
+    return UIDevice.currentDevice.let {
+        it.systemName() + " " + it.systemVersion
     }
 }
