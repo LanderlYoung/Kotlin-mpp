9aa35cc9f5ef409ddabbbcce052ac508361dff21
landerlyoung <landerlyoung@gmail.com>
2018-11-15 12:41:38 +0800

    works

--- ios/KotlinMppIos/StoryDetailViewController.swift
+++ ios/KotlinMppIos/StoryDetailViewController.swift
@@ -17,17 +17,46 @@ class StoryDetailViewController: UIViewController {
     @IBOutlet weak var webview: WKWebView!
 
     @IBOutlet weak var navigationTitle: UINavigationItem!
-    
-    var detailStory :Story? = nil
-    
+
+    var detailStory: Story!
+
+    var presenter: StoryContentPresenter!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        webview.uiDelegate
+
         // Do any additional setup after loading the view.
-        navigationTitle.title = detailStory?.title
+        navigationTitle.title = detailStory.title
+        presenter = StoryContentPresenter(storyId: detailStory.id)
+
+        presenter.onLoadingStatusChange = { [unowned self] loading in
+            print("StoryDetailViewController.onLoadingStatusChange:\(loading)")
+            return KotlinUnit()
+        }
+        presenter.onError = { [unowned self] error in
+            return KotlinUnit()
+        }
+
+        presenter.onLoadData = { [unowned self] data in
+            self.renderData(data)
+            return KotlinUnit()
+        }
+
+        presenter.onActivate()
     }
-    
-    @IBAction func back(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        presenter.onDeactivate()
+    }
+
+    private func renderData(_ data: KotlinPair) {
+        let storyContent = data.first as! StoryContent
+        let html = data.second as! String
+
+        navigationTitle.title = storyContent.title
+        webview.loadHTMLString(html, baseURL: nil)
     }
 }
